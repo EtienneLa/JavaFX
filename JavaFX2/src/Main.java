60cd97660c52dcc7f7b3655a0dded45798a72eee
EtienneLa <etienne.langensiepen@gmx.de>
2018-02-25 16:56:52 +0100

    first simple window

---
+++ JavaFX2/src/Main.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Here goes the Title");
+
+        Button button = new Button("Click me!");
+        //or button.setText("Click me!");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
