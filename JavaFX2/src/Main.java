e412b9a7f44a07025fee2037047016aaaff189cf
EtienneLa <etienne.langensiepen@gmx.de>
2018-02-25 17:26:43 +0100

    ActionHandler two buttons

--- JavaFX2/src/Main.java
+++ JavaFX2/src/Main.java
@@ -6,7 +6,10 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler<ActionEvent> {
+
+    private Button button;
+    private Button button2;
 
     public static void main(String[] args) {
         launch(args);
@@ -16,15 +19,29 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         primaryStage.setTitle("Here goes the Title");
-
-        Button button = new Button("Click me!");
+        button = new Button("Click me!");
+        button2 = new Button("Click me maybe.");
         //or button.setText("Click me!");
+        button.setOnAction(this);
+        button2.setOnAction(this);
 
         StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        layout.getChildren().addAll(button, button2);
 
         Scene scene = new Scene(layout, 300, 250);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if(event.getSource() == button) {
+            System.out.println("Hey there, button 2 is up!");
+            button.toBack();
+        }
+        if(event.getSource() == button2) {
+            System.out.println("Whats up?");
+            button.toFront();
+        }
     }
 }
